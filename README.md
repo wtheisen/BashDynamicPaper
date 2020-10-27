03ae7ad8b8bdf51faf72862e4b7e4392ac9c84ad
wtheisen <wtheisen@nd.edu>
2020-10-27 13:42:54 -0400

    Big code changes and nice readme

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # BashDynamicPaper
 Dynamic wallpaper setter written in bash
+I recommend you use it with a cron-job
+
+### Usage
+```
+USAGE: dynamicWallpaper -p [PAPER_PREFIX] -w [1]
+    -p: The prefix of your wallpaper folder without a trailing /
+    -w: Optional argument specifying whether or not to use weather data
+```
+
+### File Formatting
+There are four different time chunks to sort your wallpapers into: Morning,
+Day, Evening, and Night. This will be set dynamically based on the time of
+the sunrise and sunset in your time-zone (currently hard-coded to EST).
+
+Weather specific papers should be formated with a prefix describing the weather
+depicted in the paper. Currently the code respects 3 prefixes: "sun__", "rain___",
+and "_snow__".
+
+Ergo an example of a rainy day wallpaper might be:
+`/home/joe/Pictures/Wallpapers/Day/rain__ParkBench.png`
+
+All that matters for the time of day is the folder it's contained in, and all
+that matters for the weather is the prefix of the filename.
--- dynamicWallpaper.sh
+++ dynamicWallpaper.sh
@@ -2,11 +2,14 @@
 URL="https://api.sunrise-sunset.org/json?lat=41.6833813&lng=-86.2500066&date=today"
 JSON=$(curl -s $URL)
 
-function weather () {
-    curl "wttr.in/$1"
+function get_weather () {
+    WEATHERDATA=$(curl "wttr.in/$1?format=j1")
+
+    TEMP=$(echo $WEATHERDATA | jq '.current_condition | .[0] | .FeelsLikeF' )
+    W_TYPE=$(echo $WEATHERDATA | jq '.current_condition | .[0] | .weatherDesc | .[0] | .value')
 }
 
-function setESTTime () {
+function set_EST_time () {
     UTCSUNRISE=$(echo $JSON | jq .results.sunrise | cut -d \" -f 2 | cut -d ' ' -f 1 | sed s/:/./g | sed -r 's/(.*)\./\1/g')
     UTCSUNSET=$(echo $JSON | jq .results.sunset | cut -d \" -f 2 | cut -d ' ' -f 1 | sed s/:/./g | sed -r 's/(.*)\./\1/g')
 
@@ -21,35 +24,75 @@ function setESTTime () {
     fi
 }
 
-setESTTime 0
+function get_time_chunk () {
+    TIMEDAY=$(date +%H%M | sed 's/^0//')
+    T_TYPE="Day"
+
+    ESTSUNRISE="$1"
+    ESTSUNSET="$2"
+
+    if [[ $TIMEDAY -lt $ESTSUNRISE ]]; then
+        T_TYPE="Night"
+    elif [[ $TIMEDAY -gt $ESTSUNRISE ]] && (( TIMEDAY < ESTSUNRISE + 400 )); then
+        T_TYPE="Morning"
+    elif (( TIMEDAY < ESTSUNSET - 400 )) && (( TIMEDAY > ESTSUNRISE + 400 )); then
+        T_TYPE="Day"
+    elif (( TIMEDAY > ESTSUNSET - 400 )) && [[ $TIMEDAY < $ESTSUNSET ]]; then
+        T_TYPE="Evening"
+    else
+        T_TYPE="Night"
+    fi
+}
+
+function get_simple_weather () {
+    W_TYPE=$1
+    TEMP=$2
+
+    if [ $(echo $W_TYPE | grep "sun") ] && (( Temp > 40 )); then
+        W_TYPE="sun_"
+    elif [ $(echo $W_TYPE | grep "[rain|overcast]") ]; then
+        W_TYPE="rain_"
+    elif [ $(echo $WTYPE | grep "snow") ] || ((Temp < 40)); then
+        W_TYPE="snow_"
+    else
+        echo "";
+    fi
+}
+
+function exit_help () {
+    echo "USAGE: dynamicWallpaper -p [PAPER_PREFIX] -w [1]"
+    echo "\t-p: The prefix of your wallpaper folder without a trailing /"
+    echo "\t-w: Optional argument specifying whether or not to use weather data"
+    exit 1
+}
+
+WEATHER=0
+
+while getopts ":p:w" opts; do
+    case "${opts}" in
+        p)
+            PAPE_PREFIX=${OPTARG}
+            ;;
+        w)
+            WEATHER=1
+            ;;
+        *)
+            exit_help
+            ;;
+    esac
+done
+
+set_EST_time 0
 ESTSUNRISE=$ESTTIME
-setESTTime 1
+set_EST_time 1
 ESTSUNSET=$ESTTIME
+get_time_chunk "$ESTSUNRISE" "$ESTSUNSET"
 
-# WEATHERDATA=$(curl "wttr.in/KSBN")
+if [[ $WEATHER -ne 0 ]]; then
+    get_weather "KSBN"
+    get_simple_weather $W_TYPE $TEMP
 
-TIMEYEAR=$(date | cut -f 2,3 -d ' ')
-TIMEDAY=$(date +%H%M | sed 's/^0//')
-# TEMP=$(echo $WEATHERDATA | tail -n 4 | awk '{print $2}' | head -n 1)
-# WEATHER=$(echo $WEATHERDATA | tail -n 1 | awk '{print $3}')
-
-WTYPE="Day"
-
-# echo "$ESTSUNRISE"
-# echo "$ESTSUNSET"
-# echo "$TIMEDAY"
-
-if [[ $TIMEDAY -lt $ESTSUNRISE ]]; then
-    WTYPE="Night"
-elif [[ $TIMEDAY -gt $ESTSUNRISE ]] && (( TIMEDAY < ESTSUNRISE + 400 )); then
-    WTYPE="Morning"
-elif (( TIMEDAY -lt ESTSUNSET - 400 )) && (( TIMEDAY > ESTSUNRISE + 400 )); then
-    WTYPE="Day"
-elif (( TIMEDAY -gt ESTSUNSET - 400 )) && [[ $TIMEDAY < $ESTSUNSET ]]; then
-    WTYPE="Evening"
+    /usr/bin/feh --randomize --bg-fill "$PAPE_PREFIX"/"$T_TYPE"/"$W_TYPE"*
 else
-    WTYPE="Night"
+    /usr/bin/feh --randomize --bg-fill "$PAPE_PREFIX"/"$T_TYPE"/*
 fi
-
-echo "$WTYPE"
-/usr/bin/feh --randomize --bg-fill /home/wtheisen/Dropbox/Wallpapers/"$WTYPE"/*
